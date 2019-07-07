b06b118b3ebae602910258c255c5bf28a7c2b631
German Distel <germandistel@gmail.com>
2019-07-07 13:56:02 -0300

    Updated whitenoise path for staticfile storage in the settings

--- product_sparrer/settings/production.py
+++ product_sparrer/settings/production.py
@@ -4,7 +4,7 @@ import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ALLOWED_HOSTS = []
 
