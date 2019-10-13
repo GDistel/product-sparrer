e9d4ce7e1abd44eb3afd9f510902834879ecb9c0
German Distel <germandistel@gmail.com>
2019-10-12 21:12:05 -0300

    Added password hashing in users view in Django

--- django-back-end/product_sparrer/users/serializers.py
+++ django-back-end/product_sparrer/users/serializers.py
@@ -4,8 +4,18 @@ from .models import CustomUser
 from product_sparrer.tickets.models import TicketsModel
 
 class UserSerializer(serializers.ModelSerializer):
-    tickets = serializers.PrimaryKeyRelatedField(many=True, queryset=TicketsModel.objects.all())
+    tickets = serializers.PrimaryKeyRelatedField(many=True, queryset=TicketsModel.objects.all(), required=False)
 
     class Meta:
         model = CustomUser
-        fields = ('id', 'username', 'email', 'tickets')
+        fields = ('id', 'username', 'email', 'password', 'tickets')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    # def create(self, validated_data):
+    #     user = CustomUser.objects.create(
+    #         email = validated_data['email'],
+    #         username = validated_data['username'],
+    #         password = make_password(validated_data['password']
+    #         tickets = validated_data['tickets'])
+    #     )
+    #     return user
--- django-back-end/product_sparrer/users/views.py
+++ django-back-end/product_sparrer/users/views.py
@@ -1,8 +1,12 @@
 from rest_framework import viewsets
 from product_sparrer.users.serializers import UserSerializer
+from django.contrib.auth.hashers import make_password
 from .models import CustomUser
 
 
 class UsersViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all().order_by('-id')
     serializer_class = UserSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(tickets=[], password=make_password(self.request.data.get("password")))
