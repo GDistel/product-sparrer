5a96160d9688336ef85bfa1e840d4df7b68fdc64
German Distel <germandistel@gmail.com>
2019-11-21 19:50:10 -0300

    Adjusted prod settings in django for broker

--- django-back-end/product_sparrer/settings/production.py
+++ django-back-end/product_sparrer/settings/production.py
@@ -10,6 +10,15 @@ ALLOWED_HOSTS = [
     'product-sparrer.herokuapp.com',
 ]
 
+CELERY_BROKER_URL = os.environ.get('CLOUDAMQP_URL')
+CELERY_BROKER_POOL_LIMIT = 1
+CELERY_BROKER_HEARTBEAT = None # We're using TCP keep-alive instead
+CELERY_BROKER_CONNECTION_TIMEOUT = 30 # May require a long timeout due to Linux DNS timeouts etc
+CELERY_RESULT_BACKEND = None # AMQP is not recommended as result backend as it creates thousands of queues
+CELERY_EVENT_QEUE_EXPIRES = 60 # Will delete all celeryev. queues without consumers after 1 minute.
+CELERY_WORKER_PREFETCH_MULTIPLIER = 1 # Disable prefetching, it's causes problems and doesn't help performance
+CELERY_WORKER_CONCURRENCY = 50 # If you tasks are CPU bound, then limit to the number of cores, otherwise increase substainally
+
 REST_FRAMEWORK['DEFAULT_AUTHENTICATION_CLASSES'] = ('rest_framework.authentication.TokenAuthentication',)
 
 CORS_ORIGIN_WHITELIST = (
