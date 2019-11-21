9f4af43f92aa34788c05665ad4bf7201fe5a93ae
German Distel <germandistel@gmail.com>
2019-11-21 18:31:54 -0300

    Commented off sendgrid email sender

--- django-back-end/product_sparrer/utils/email.py
+++ django-back-end/product_sparrer/utils/email.py
@@ -1,22 +1,23 @@
 import os
 from django.core.mail import send_mail
 from django.conf import settings
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
 
-def send_email(destinatary='product-sparrer@mailinator.com', subject=' ', body='Empty'):
-    message = Mail(
-        from_email='noreply@product-sparrer.com',
-        to_emails= destinatary,
-        subject=subject,
-        html_content=body
-    )
-    try:
-        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
-        response = sg.send(message)
-        print(response.status_code)
-    except Exception as e:
-        print(str(e))
+# from sendgrid import SendGridAPIClient
+# from sendgrid.helpers.mail import Mail
+#
+# def send_email(destinatary='product-sparrer@mailinator.com', subject=' ', body='Empty'):
+#     message = Mail(
+#         from_email='noreply@product-sparrer.com',
+#         to_emails= destinatary,
+#         subject=subject,
+#         html_content=body
+#     )
+#     try:
+#         sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+#         response = sg.send(message)
+#         print(response.status_code)
+#     except Exception as e:
+#         print(str(e))
 
 def send_gmail(destinatary='product-sparrer@mailinator.com', subject=' ', body='Empty'):
     email_from = settings.EMAIL_HOST_USER
